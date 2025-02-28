00ad5b098368862fdc108475772d5ee7fd1a401d
BattleGear04 <karekarayoosh@gmail.com>
2025-02-28 12:49:15 +0530

    added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='page' >
+        <header>SentiSense</header>
+
+        <div className='main'>
+          <div className='intro'>
+            <div className='about'>
+              <h1>Unlock the Power of <span className='decorator'>Sentiment Analysis</span></h1>
+              <p>Paste a link, and let our AI analyze the sentiment behind the comments!
+                 Whether it's a product review, social media post, or blog discussion, we break down emotions,
+                highlight key opinions, and give you valuable insights—instantly. 🚀</p>
+            </div>
+            <div className='image'></div>
+          </div>
+          <form >
+            <input type='text' placeholder='Enter post URL...' required/>
+            <button type='submit'>Analyse</button>
+          </form>
+          
+        </div>
+
+        <footer>
+          © 2025 SentiSense. All Rights Reserved.
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
